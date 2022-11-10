35fa596a4e3399d92c0ac4350070b49500ab43ee
gcasado0 <casado.gustavo@gmail.com>
2022-11-09 23:48:58 -0300

    unidad 3 - 20221109

--- unidad3/utils.py
+++ unidad3/utils.py
@@ -1,19 +1,23 @@
-from importlib.resources import Package
-
-
-from time import time
-
-def medir(f):
-  def fmedida(x,y):
-    inicio = time()
-    retorno = f(x,y)
-    fin = time()
-    print(fin-inicio)
-    return retorno
-  return fmedida
 
 def esta_ordenada(lista):
   for i in range(len(lista)-1):
-      if lista[i]>lista[i+1]:
-          return False
+    if lista[i]>lista[i+1]:
+      return False
   return True
+
+from time import time
+def medir(f):
+  def fmedida(x):
+    inicio = time()
+    retorno = f(x)
+    fin = time()
+    print("Elapsed time: {:0.6f}".format(fin-inicio))
+    return retorno
+  return fmedida
+
+import random
+def listaAleatorios(n):
+  lista = [0]  * n
+  for i in range(n):
+      lista[i] = random.randint(0, 1000)
+  return lista  
